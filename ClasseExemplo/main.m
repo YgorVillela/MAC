96db7effa56f3f80999162f231f7910bcfb538e9
Usuário Convidado <Guest@l2602micro27.local>
2019-03-18 08:33:31 -0300

    Initial Commit

---
+++ ClasseExemplo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ClasseExemplo
+//
+//  Created by Usuário Convidado on 18/03/19.
+//  Copyright © 2019 Ygor Villela. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
